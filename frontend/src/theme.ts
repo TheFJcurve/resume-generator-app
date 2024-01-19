b2c64fbce516681b3a9ed763aa024b327ba74873
Sargun <sargunbhatti27@gmail.com>
2024-01-18 20:14:09 -0500

    all data fields are being posted now

--- frontend/src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeConfig, extendTheme } from "@chakra-ui/react";
-
-const config: ThemeConfig = {
-    initialColorMode: 'dark'
-}
-
-const theme = extendTheme({ config });
-
-export default theme
