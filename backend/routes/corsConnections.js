d0d073c15f872fda00b9f9043548384c5b0561c8
Sargun <sargunbhatti27@gmail.com>
2024-02-21 12:55:20 -0500

    added pdflatex to backend

--- backend/routes/corsConnections.js
+++ backend/routes/corsConnections.js
@@ -13,16 +13,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: allowedOrigins,
   })
 );
 
