e8b589e06b2a62a6128092ce8f31d101f855da4e
Sargun <sargunbhatti27@gmail.com>
2024-02-21 12:45:08 -0500

    bug fixes p2

--- backend/routes/corsConnections.js
+++ backend/routes/corsConnections.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://resume-generator-app-rosy.vercel.app/",
+  "https://resume-generator-app-rosy.vercel.app",
 ];
 
 app.use(
