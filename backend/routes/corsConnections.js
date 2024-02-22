22828dd89b4015516d33851a1007115b4b01e544
Sargun <sargunbhatti27@gmail.com>
2024-02-22 00:08:24 -0500

    modified backend to look good

--- backend/routes/corsConnections.js
+++ backend/routes/corsConnections.js
@@ -2,15 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const resumeRoutes = require("./resumes");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://resume-generator-app-rosy.vercel.app",
-  "https://resume-generator-gjnze8uca-sargun-singh-bhattis-projects.vercel.app",
-];
+const allowedOrigins = ["http://localhost:5173"];
 
 app.use(
   cors({
@@ -21,28 +18,8 @@ app.use(
 // Setting up the Routes
 app.use("/api/resumes", resumeRoutes);
 
-app.use((req, res, next) => {
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Resume Generator</title>
-    <style>
-      body {
-        background-color: black;
-        color: white;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Resume Generator Backend</h1>
-  </body>
-  </html>`);
-  console.log(req.path, req.method);
-  next();
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname + "/webpage.html"));
 });
 
 module.exports = app;
