d926df93741e7f48cba7d65c41d80a608d150394
Sargun <sargunbhatti27@gmail.com>
2024-02-25 18:30:53 -0500

    dockerized frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  }
 })
