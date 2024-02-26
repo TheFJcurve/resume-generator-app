ac6b4bfd97d7f55de4803c28f445a2d8a7944226
Sargun <sargunbhatti27@gmail.com>
2024-02-26 10:27:34 -0500

    added a good error page

--- frontend/src/components/design/ErrorPage.tsx
+++ frontend/src/components/design/ErrorPage.tsx
@@ -1,5 +1,23 @@
+import { Flex, HStack, Heading, Image } from "@chakra-ui/react";
+import errorGif from "../../assets/arpitBala.gif";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <Flex
+      direction={"column"}
+      justify={"center"}
+      align={"center"}
+      minHeight={"100vh"}
+      gap={10}
+    >
+      <HStack>
+        <Heading>The page you</Heading>
+        <Heading color={"teal.300"}> requested </Heading>
+        <Heading>was not found</Heading>
+      </HStack>
+      <Image src={errorGif} alt="404 error" width={"600px"} borderRadius={10} />
+    </Flex>
+  );
 };
 
 export default ErrorPage;
